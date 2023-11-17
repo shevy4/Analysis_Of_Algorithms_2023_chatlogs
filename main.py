95c22ca94f7e5a11748b0e5e3c4ee2e8d331501b
shav <61606455+shevy4@users.noreply.github.com>
2023-11-17 00:42:46 -0500

    Finished Draft #1

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 from collections import defaultdict
+import tableprint as tb
+import tqdm
+from time import sleep
 
 logs = [["08:00:13 From Jack Johnson: hello to everyone"],
         ["08:00:21 From Emily Smith: hello"],
         ["08:00:29 From Chris Brown: hello"],
         ["08:00:34 From Chris Brown: hi again everyone!"]]
 
+# Init of default dictionary, this handles key errors arising from a normal dictionary
 chatlog = defaultdict(list)
-message = []
 
 
+# This Function returns the average frequency of messages sent by a particular student
+def parse_grade(name):
+    return str((len(chatlog.get(name.casefold())) / len(logs)) * 100) + "%"
+
+
+# This function splits the chat-log in reorganizes it by indexing
 def parse_chat():
     for line in logs:
         chatlog[line[0].split(" ")[2].casefold() + " " + line[0].split(" ")[3].casefold().strip(":")].append(
             line[0].split(" ")[0] + line[0].split(":")[3])
 
 
+# Simple loading bar for aesthetics
+def loading_animation(length):
+    for _ in tqdm.tqdm(range(length), desc="Grading..."):
+        sleep(.5)
+
+
+# This function displays the student name, number of messages and the message sent
 def display_chats(name):
-    global message
+    parse_chat()
+    if name.casefold() == "exit":
+        exit(0)
+
     message = []
+    data = []
+    # Iterates chat-log using O(n) complexity
     for key, value in chatlog.items():
         if name.casefold() == str(key).casefold():
             message = value
+            break
+    # If the message length is 0 then student name is not found in chat
     if len(message) == 0:
         print("Invalid Name, Not Found")
+        chatlog.clear()
         return
+
+    # Displays student info in a table
+    headers = ["Message Frequency", "Message"]
     for x, y in enumerate(message):
-        print("[" + str(x + 1) + "] " + y)
-    parse_grade(name)
-
-
-def parse_grade(name):
-    print((len(chatlog.get(name.casefold())) / len(logs)) * 100, "%")
-
-
-def main():
-    display_chats(input("Enter Name To Search\n"))
+        data.append(["[" + str(x + 1) + "]"] + [y])
+    loading_animation(len(logs))
+    data.append(["Participation Grade : "] + [str(parse_grade(name))])
+    tb.banner("Student Name : " + name, 1)
+    tb.table(data, headers)
+    chatlog.clear()
 
 
 if __name__ == '__main__':
-    parse_chat()
-    main()
+    while True:
+        display_chats(input("Enter Name To Search or 'exit' to exit\n"))
